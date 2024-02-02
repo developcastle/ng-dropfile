a4d16d7bbd0efac6ad77b98f6bd5fcb4a71aedda
danielcn <danielcn.9003@gmail.com>
2024-02-02 10:57:48 -0600

    Removed app component

--- src/app/dropfile.module.ts
+++ src/app/dropfile.module.ts
@@ -1,31 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { DropfileComponent } from './ng-dropfile/ng-dropfile.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgIconsModule } from '@ng-icons/core';
-import { featherUploadCloud, featherFile ,featherTrash2,featherImage} from '@ng-icons/feather-icons';
-
+import {
+  featherUploadCloud,
+  featherFile,
+  featherTrash2,
+  featherImage,
+} from '@ng-icons/feather-icons';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DropfileComponent
-  ],
+  declarations: [DropfileComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
-    NgIconsModule.withIcons({ featherUploadCloud ,featherFile,featherTrash2,featherImage}),
+    NgIconsModule.withIcons({
+      featherUploadCloud,
+      featherFile,
+      featherTrash2,
+      featherImage,
+    }),
   ],
-  providers: [
-    
-  ],
-  exports:[
-    DropfileComponent
-  ],
-  bootstrap: [AppComponent]
+  exports: [DropfileComponent],
 })
-export class NgDropfileModule { }
+export class NgDropfileModule {}
